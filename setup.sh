fae61724d3131aacaba36d3e8f05ff1883adb9ee
ThaysonScript <thaysonguedes01@gmail.com>
2025-02-09 00:33:44 -0300

    baixar pdfs de minuta

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-python3 -m venv venv
+if [[ ! -d "venv" ]]; then
+    python3 -m venv env
+
+    python -m ipykernel install --user --name=env --display-name "virtual_env_personalizado"
+
+fi
 
 source env/bin/activate
 
-pip install -r requirements.txt
+pip install --upgrade pip
 
-python -m ipykernel install --user --name=env --display-name "virtual_env_personalizado"
+pip install -r requirements.txt
